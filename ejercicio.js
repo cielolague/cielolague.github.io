d7dfe47e7e89edc5c4ce014044091d97d565ab85
cielolague <162398409+cielolague@users.noreply.github.com>
2024-03-12 18:21:36 -0600

    Add files via upload

---
+++ ejercicio.js
@@ -0,0 +1,15 @@
+function cambiaColor()
+{
+   variable_album = document.getElementById("album");
+   console.log(variable_album);
+   variable_album.style.backgroundColor = "blue";
+}
+
+function cambiaTamano()
+{
+   variable_imagen = document.getElementsByClassName("imagen");
+   console.log(variable_imagen);
+   variable_imagen:avatar.style.width = "100px";
+   variable_imagen:avatar.style.height = "100px";
+
+}
