2d28507c34deb52a2e99079fd7c98c7a7df0d8ef
cielolague <162398409+cielolague@users.noreply.github.com>
2024-03-12 18:05:22 -0600

    ejercicio.js

--- ejercicio.js
+++ ejercicio.js
@@ -7,9 +7,9 @@ function cambiaColor()
 
 function cambiaTamano()
 {
-   variable_imagen = document.getElementsByClassName("Aang");
+   variable_imagen = document.getElementsByClassName("imagen");
    console.log(variable_imagen);
-   variable_imagen:variable_album.style.width = "1000px";
-   variable_imagen:variable_album.style.height = "1000px";
+   variable_imagen:avatar.style.width = "100px";
+   variable_imagen:avatar.style.height = "100px";
 
 }
